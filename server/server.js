07d478150da41c05340cfaa83d2ce68602874003
root <agaillard88@gmail.com>
2021-06-24 11:07:38 -0400

    Cleaned up code for refactor

--- server/server.js
+++ server/server.js
@@ -12,54 +12,44 @@ const client = new Client({
 })
 client.connect()
 
+streamQuestions = () => {
+  const fileName = './../csv/questions.csv';
+  const filePath = path.join(__dirname, fileName);
+
+  const readStream = fs.createReadStream(filePath, { encoding: 'utf8' });
+
+  let count = 0;
+  let lineRemainder = '';
+
+  readStream.on('data', (chunk) => {
+    readStream.pause();
+    count++;
+    let lines = chunk.split('\n');
+    lines[0] = lineRemainder + lines[0];
+    lineRemainder = lines.pop();
+    let firstLine = lines[0];
 
 
-var streamData = function() {
-  return new Promise((resolve, reject) => {
-    const fileName = 'questions.csv';
-    const filePath = path.join(__dirname, fileName);
+    lines.forEach((line) => {
+      const values = line.split(',');
+      const id = values[0];
+      const product_id = values[1];
+      const question_body = values[2];
+      const unixTimestamp = values[3];
+      const asker_name = values[4];
+      const asker_email = values[5];
+      const reported = values[6];
+      const question_helpfulness = values[7];
+      const milliseconds = unixTimestamp * 1000;
+      const dateObject = new Date(milliseconds);
+      const question_date = dateObject.toLocaleString();
 
-    const readStream = fs.createReadStream(filePath, { encoding: 'utf8' });
-
-    let count = 0;
-    let lineRemainder = '';
-
-    readStream.on('data', (chunk) => {
-      readStream.pause();
-      count++;
-      let lines = chunk.split('\n');
-      lines[0] = lineRemainder + lines[0];
-      lineRemainder = lines.pop();
-      let firstLine = lines[0];
-
-
-      lines.forEach((line) => {
-        const values = line.split(',');
-        const id = values[0];
-        const product_id = values[1];
-        const question_body = values[2];
-        const unixTimestamp = values[3];
-        const asker_name = values[4];
-        const asker_email = values[5];
-        const reported = values[6];
-        const question_helpfulness = values[7];
-        const milliseconds = unixTimestamp * 1000;
-        const dateObject = new Date(milliseconds);
-        const question_date = dateObject.toLocaleString();
-
-        console.log(id, product_id, question_body, question_date, asker_name, asker_email, reported, question_helpfulness);
-      })
-
-      resolve(readStream);
+      console.log(id, product_id, question_body, question_date, asker_name, asker_email, reported, question_helpfulness);
     })
-
-  }).then((readStream) => {
-    console.log('resumed');
-    streamData();
-  });
+  })
 }
 
-streamData();
+streamQuestions();
 
 
 
